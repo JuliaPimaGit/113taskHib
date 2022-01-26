eb39f64a1557b44927340e6fbaa78013ae954a7c
Julia Pimmahova <pimahovayulia@gmail.com>
2022-01-26 11:19:04 +0300

    Task 113. Version 01

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+
+import java.sql.SQLException;
+
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Sasdkfh", "Pim", (byte) 78);
+        userService.saveUser("Yesllh", "Pim", (byte) 45);
+        userService.saveUser("HFl", "Pim", (byte) 55);
+        userService.saveUser("IHF", "Pim", (byte) 21);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,79 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
-
     }
 
-    public void createUsersTable() {
+    Connection connection = Util.getConnection();
 
+    public void createUsersTable() {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS schema_113.user (id BIGINT PRIMARY KEY AUTO_INCREMENT, name varchar(100), lastName varchar(255), age smallint)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS schema_113.user");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-
+    public void saveUser(String name, String lastName, byte age){
+        try (PreparedStatement pst = connection.prepareStatement("INSERT INTO schema_113.user (name, lastName, age) VALUES (?,?,?)")) {
+            pst.setString(1, name);
+            pst.setString(2, lastName);
+            pst.setByte(3, age);
+            pst.executeUpdate();
+            System.out.println("User с именем – "+ name +" добавлен в базу данных");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
-    }
+          try (PreparedStatement pst = connection.prepareStatement("DELETE FROM schema_113.user WHERE id=?")) {
+              pst.setLong(1,id);
+          } catch (SQLException e) {
+              e.printStackTrace();
+          }
+        }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = new ArrayList<>();
+        try(Statement statement = connection.createStatement()) {
+            statement.execute("SELECT * FROM schema_113.user");
+            ResultSet resultSet = statement.getResultSet();
+            while (resultSet.next()){
+                long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("lastName");
+                byte age = resultSet.getByte("age");
+                User user = new User(name, lastName, age);
+                list.add(user);
+                System.out.println(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
     public void cleanUsersTable() {
-
+        try(Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS schema_113.user");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
